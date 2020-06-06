96f4fd090012023b92206e0675d5a5cc0beb660e
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-06-06 01:15:44 -0300

    deploy v6

--- common/environment.ts
+++ common/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  db: process.env.DATABASE_URL || '',
+  env: process.env.NODE_ENV || 'development'
 }
--- knexfile.ts
+++ knexfile.ts
@@ -1,15 +1,30 @@
 import path from 'path'
+import { environment } from './common/environment';
 
 module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    },
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    },
+    useNullAsDefault: true
   },
-  migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
-  },
-  seeds: {
-    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
-  },
-  useNullAsDefault: true
+  production: {
+    client: 'pg',
+    connection: environment.db + '?ssl=true',
+
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    },
+    useNullAsDefault: true
+  }
 }
