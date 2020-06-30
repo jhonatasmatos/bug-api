dadb5ab3058885fd31f5f96f1834c036c79e3b79
Jhonatas Santos <jhonatassantos@MacBook-Pro-de-Jhonatas.local>
2020-06-29 23:51:24 -0300

    update package.json/tsconfig and config eslint

--- common/environment.ts
+++ common/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   port: process.env.PORT || 3000,
   db: process.env.DATABASE_URL || '',
-  env: process.env.NODE_ENV || 'development'
-}
+  env: process.env.NODE_ENV || 'development',
+};
